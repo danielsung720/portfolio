0678f2f1d724c3283c718931c2755d10e9fddc34
Daniel <activearmy720@gmail.com>
2020-06-10 11:32:06 +0800

    Updates

---
+++ precache-manifest.ab186c3315ecd2732f9735c64c8ef1f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32a7bdcd9b806bfaa57441899a9f945",
+    "url": "/my_resume/index.html"
+  },
+  {
+    "revision": "10b384bfb564766b2bd1",
+    "url": "/my_resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a064feb6e94532985f5",
+    "url": "/my_resume/static/js/2.f9981d91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_resume/static/js/2.f9981d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b384bfb564766b2bd1",
+    "url": "/my_resume/static/js/main.7e2ce917.chunk.js"
+  },
+  {
+    "revision": "f50c0e12acd3d35221b8",
+    "url": "/my_resume/static/js/runtime-main.4d44b889.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
