d688c54e0130f94232588e9e62b0ec70d2cb22a0
Daniel <activearmy720@gmail.com>
2020-06-16 00:57:31 +0800

    Updates

---
+++ precache-manifest.f9177c2e56d9a02925f3d6c01f036984.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4c10d576329eec0ed9bdc1b6fcab00",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "3a006cbfe07ff36516eb",
+    "url": "/resume/static/css/main.0ea078ed.chunk.css"
+  },
+  {
+    "revision": "438883e62b91e5b4e5f3",
+    "url": "/resume/static/js/2.4455f1f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.4455f1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a006cbfe07ff36516eb",
+    "url": "/resume/static/js/main.182d96d7.chunk.js"
+  },
+  {
+    "revision": "f0153ca5b4baadf970fa",
+    "url": "/resume/static/js/runtime-main.b9fe569b.js"
+  },
+  {
+    "revision": "93fe7c3ebb1f85002429a15fc32eb7eb",
+    "url": "/resume/static/media/logo.93fe7c3e.png"
+  },
+  {
+    "revision": "ca4465e60fadae07539536952e39eef3",
+    "url": "/resume/static/media/resume_head_shot.ca4465e6.jpg"
+  }
+]);
