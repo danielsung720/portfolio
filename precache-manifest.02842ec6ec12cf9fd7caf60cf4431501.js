7999ae5dbdd29d97fe0cf82ac2453866f497cf93
Daniel <activearmy720@gmail.com>
2020-06-16 22:33:12 +0800

    Updates

---
+++ precache-manifest.02842ec6ec12cf9fd7caf60cf4431501.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54ceecaa5088057aff3002506680cb6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "43dfb57298342e24227f",
+    "url": "/portfolio/static/css/main.c95c7556.chunk.css"
+  },
+  {
+    "revision": "eeccc30b002913c65b31",
+    "url": "/portfolio/static/js/2.793afbff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.793afbff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dfb57298342e24227f",
+    "url": "/portfolio/static/js/main.afecb186.chunk.js"
+  },
+  {
+    "revision": "2739a80f03459a22b6db",
+    "url": "/portfolio/static/js/runtime-main.768ea7a4.js"
+  },
+  {
+    "revision": "d6ce2f9951ce6eea198f4860f43dde17",
+    "url": "/portfolio/static/media/goback.d6ce2f99.png"
+  },
+  {
+    "revision": "125677b2759b93ff65dace3ea91684a5",
+    "url": "/portfolio/static/media/gotop.125677b2.png"
+  },
+  {
+    "revision": "93fe7c3ebb1f85002429a15fc32eb7eb",
+    "url": "/portfolio/static/media/logo.93fe7c3e.png"
+  },
+  {
+    "revision": "ca4465e60fadae07539536952e39eef3",
+    "url": "/portfolio/static/media/resume_head_shot.ca4465e6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c1629c75c4518eb5f08f2f2e9ebb4275.js"
+  "/portfolio/precache-manifest.02842ec6ec12cf9fd7caf60cf4431501.js"
 );
 
 self.addEventListener('message', (event) => {
